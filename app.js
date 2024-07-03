ca53e7450d556579fefb1c6af0691496e6689915
Aliza Khan <alizaadi2@gmail.com>
2024-07-04 02:03:57 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var quizQuestions = [
+    {
+        Qnum : "01",
+        question: "HTML stands for ?",
+        option1: "HyperText Machine Language",
+        option1: "HyperText Markup Language",
+        option1: "HyperText Man-Made Language",
+        option1: "HyperText & Links Markup Language",
+
+    }
+]
