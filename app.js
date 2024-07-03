92382c812e53dbb0759788b078d8215b9692c38a
Aliza Khan <alizaadi2@gmail.com>
2024-07-04 02:09:37 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,92 @@
 var quizQuestions = [
     {
-        Qnum : "01",
+        qnum : "01",
         question: "HTML stands for ?",
         option1: "HyperText Machine Language",
         option1: "HyperText Markup Language",
         option1: "HyperText Man-Made Language",
         option1: "HyperText & Links Markup Language",
-
-    }
+        correctAnswer: "HyperText Markup Language"
+    },
+    {
+        qnum : "02",
+        question: "HTML stands for ?",
+        option1: "HyperText Machine Language",
+        option1: "HyperText Markup Language",
+        option1: "HyperText Man-Made Language",
+        option1: "HyperText & Links Markup Language",
+        correctAnswer: "HyperText Markup Language"
+    },
+    {
+        qnum : "03",
+        question: "HTML stands for ?",
+        option1: "HyperText Machine Language",
+        option1: "HyperText Markup Language",
+        option1: "HyperText Man-Made Language",
+        option1: "HyperText & Links Markup Language",
+        correctAnswer: "HyperText Markup Language"
+    },
+    {
+        qnum : "04",
+        question: "HTML stands for ?",
+        option1: "HyperText Machine Language",
+        option1: "HyperText Markup Language",
+        option1: "HyperText Man-Made Language",
+        option1: "HyperText & Links Markup Language",
+        correctAnswer: "HyperText Markup Language"
+    },
+    {
+        qnum : "05",
+        question: "HTML stands for ?",
+        option1: "HyperText Machine Language",
+        option1: "HyperText Markup Language",
+        option1: "HyperText Man-Made Language",
+        option1: "HyperText & Links Markup Language",
+        correctAnswer: "HyperText Markup Language"
+    },
+    {
+        qnum : "06",
+        question: "HTML stands for ?",
+        option1: "HyperText Machine Language",
+        option1: "HyperText Markup Language",
+        option1: "HyperText Man-Made Language",
+        option1: "HyperText & Links Markup Language",
+        correctAnswer: "HyperText Markup Language"
+    },
+    {
+        qnum : "07",
+        question: "HTML stands for ?",
+        option1: "HyperText Machine Language",
+        option1: "HyperText Markup Language",
+        option1: "HyperText Man-Made Language",
+        option1: "HyperText & Links Markup Language",
+        correctAnswer: "HyperText Markup Language"
+    },
+    {
+        qnum : "08",
+        question: "HTML stands for ?",
+        option1: "HyperText Machine Language",
+        option1: "HyperText Markup Language",
+        option1: "HyperText Man-Made Language",
+        option1: "HyperText & Links Markup Language",
+        correctAnswer: "HyperText Markup Language"
+    },
+    {
+        qnum : "09",
+        question: "HTML stands for ?",
+        option1: "HyperText Machine Language",
+        option1: "HyperText Markup Language",
+        option1: "HyperText Man-Made Language",
+        option1: "HyperText & Links Markup Language",
+        correctAnswer: "HyperText Markup Language"
+    },
+    {
+        qnum : "10",
+        question: "HTML stands for ?",
+        option1: "HyperText Machine Language",
+        option1: "HyperText Markup Language",
+        option1: "HyperText Man-Made Language",
+        option1: "HyperText & Links Markup Language",
+        correctAnswer: "HyperText Markup Language"
+    },
 ]
